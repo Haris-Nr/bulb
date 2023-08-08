a5ce1681a2f9661b1da326d622c0cbc41ebda455
Haris <121281257+Haris-Nr@users.noreply.github.com>
2023-08-08 09:57:04 +0500

    firs commit

---
+++ light.js
@@ -0,0 +1,7 @@
+function lighton() {
+    document.getElementById("bulb").src="../images/pic_bulbon.gif";
+}
+
+function lightof() {
+    document.getElementById("bulb").src="../images/pic_bulboff.gif";
+}
